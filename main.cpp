c8de54274359e11392f371171ab878ddb5703db5
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-14 22:41:49 +0300

    ...

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    system("chcp 1251>nul");
+    cout << "Программа на C++" << endl;
+    system("pause>nul");
+    return 0;
+}
