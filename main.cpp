45fc4bfff76c99980f238629749c67122c803698
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-14 22:49:06 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <cstdlib>
 
 using namespace std;
 
 int main(){
-    system("chcp 1251>nul");
-    cout << "Программа на C++" << endl;
-    system("pause>nul");
+    const double kmInMile = 1.609344;
+    double distMile, distKm;
+    cout << "Укажите расстояние в милях - ";
+    cin >> distMile;
+    distKm = distMile * kmInMile;
+    cout << "Расстояние в км - " << distKm << endl;
     return 0;
 }
