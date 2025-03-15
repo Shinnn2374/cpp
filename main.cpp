138756a54c1501c050762cb9ed660c9aec7fdef8
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-15 20:46:46 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main(){
     int n;
     int ar[n];
-    cout << "Введите число"
+    cout << "Введите число - ";
+    cin >> n;
+    for (int  i = 0;  i < n; ++ i) {
+        if (i%2){
+            ar[i] =i;
+        }
+    }
     return 0;
 }
