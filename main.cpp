387c2f6dcf130dc936b3a46366cd9946b50e9fb3
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-15 20:40:15 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -2,18 +2,9 @@
 
 using namespace std;
 
-int getMileInkM(){
-    int mile, km;
-    cout << "Введите количество км - ";
-    cin >> mile;
-    const double kmInMile = 1.609344;
-    km = mile * kmInMile;
-    cout << km;
-    return km;
-}
-
-
 int main(){
-    getMileInkM();
+    int n;
+    int ar[n];
+    cout << "Введите число"
     return 0;
 }
