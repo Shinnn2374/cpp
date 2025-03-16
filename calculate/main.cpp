e4062a8ee3c451f379912411820b4b84d3b1cb50
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-16 18:08:32 +0300

    ...

--- calculate/main.cpp
+++ calculate/main.cpp
@@ -2,17 +2,51 @@
 
 using namespace std;
 
+int sum(int firstNum, int secondNum){
+    cout << "Сумма равна - " << firstNum + secondNum;
+    return firstNum + secondNum;
+}
+
+int subtraction(int firstNum, int secondNum){
+    cout << "Вычитание равно - " << firstNum - secondNum;
+    return firstNum - secondNum;
+}
+
+int multiplication(int firstNum, int secondNum){
+    cout << "Умножение равно - " << firstNum * secondNum;
+    return firstNum * secondNum;
+}
+
+int division(int firstNum, int secondNum){
+    cout << "Деление равна - " << firstNum / secondNum;
+    return firstNum / secondNum;
+}
+
 int main() {
-    int n;
-    cout << "Введите число - ";
-    cin >> n;
-    int len = (n + 1) / 2;
-    int numbers[len];
-    for (int i = 0; i <= len; i++) {
-        numbers[i] = 2 * i;
+    int operation, firstNum, secondNum;
+    cout << "Введите первое число - ";
+    cin >> firstNum;
+    cout << "Введите второе число - ";
+    cin >> secondNum;
+    cout << "1.Сложение" << endl;
+    cout << "2.Вычитание" << endl;
+    cout << "3.Умножение" << endl;
+    cout << "4.Деление" << endl;
+    cout << "Выберите операцию(Введите число) - ";
+    cin >> operation;
+
+    switch (operation) {
+        case 1:
+            sum(firstNum, secondNum);
+            break;
+        case 2:
+            div(firstNum, secondNum);
+            break;
+        case 3:
+            multiplication(firstNum,secondNum);
+            break;
+        case 4:
+            div(firstNum,secondNum);
+            break;
     }
-    for (int i = 0; i < len; i++) {
-        cout << numbers[i] << endl;
-    }
-    return 0;
 }
