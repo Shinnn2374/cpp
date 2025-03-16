e04bc50906e82ee0dda2f021ad7c771e39886c50
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-16 14:34:34 +0300

    ...

--- main.cpp
+++ main.cpp
@@ -2,18 +2,17 @@
 
 using namespace std;
 
-int main(){
+int main() {
     int n;
     cout << "Введите число - ";
     cin >> n;
-    int len = n / 2 - 1;
+    int len = (n + 1) / 2;
     int numbers[len];
-    for (int i = 0; i <= len; i++) {
-        if (i % 2){
-            numbers[i] = i;
-        }
+    for (int i = 0; i < len; i++) {
+        numbers[i] = 2 * i + 1;
     }
-    for (int i = 0; i <= len; i++) {
+    for (int i = 0; i < len; i++) {
         cout << numbers[i] << endl;
     }
+    return 0;
 }
